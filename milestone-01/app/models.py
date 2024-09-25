38426f2e0e3467f1dbbaa8b68cc185612e82c617
rajesh <rajesh.jangid@one2n.in>
2024-09-25 07:54:54 +0530

    Update migration

--- milestone-01/app/models.py
+++ milestone-01/app/models.py
@@ -1,11 +1,11 @@
 from . import db
 
-
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     age = db.Column(db.Integer, nullable=False)
     phone_number = db.Column(db.Integer, nullable=False)
+   # gender = db.Column(db.String(100), nullable=False) # New field Added
     
 
     def __init__(self, name, age, phone_number):
